93151a65f103ad5059abe6183a36f2dd2448bf57
YahyaBazzalDev <152005421+YahyaBazzalDev@users.noreply.github.com>
2023-11-25 17:06:07 +0200

    Add files via upload

---
+++ WfifCracker.py
@@ -0,0 +1,13 @@
+import subprocess
+NetInput = input("Enter the name of the network you want to crack: ")
+if NetInput == "":
+    print("Network not found")
+    exit()
+network_name = NetInput
+result = subprocess.run(['netsh', 'wlan', 'show', 'profile', network_name, 'key=clear'], stdout=subprocess.PIPE)
+output = result.stdout.decode()
+print(f"Network Password: {output}")
+for line in output.split('\n'):
+    if "Key Content" in line:
+        print(f"The password is: {result.stdout.decode().split(':')[1].strip()}")
+        print(line.split(":")[1].strip())
